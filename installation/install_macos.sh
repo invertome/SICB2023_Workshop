d6ca635626176cda6c0e562ae510c0813fb6e330
InverTome <104219631+invertome@users.noreply.github.com>
2022-12-14 14:00:34 -0700

    Update install_macos.sh

--- installation/install_macos.sh
+++ installation/install_macos.sh
@@ -158,8 +158,11 @@ fi
 #clone the tiammat repository
 git clone https://github.com/mtassia/TIAMMAt.git
 
+#clone the workshop materials
+git clone https://github.com/invertome/SICB2023_Workshop.git
+
 #add the location of python, miniconda3, and tiammat to path
-export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin:$HOME/miniconda3/bin:$folder/TIAMMAt
+export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin:$HOME/miniconda3/bin:$folder/TIAMMAt:$folder/SICB2023_Workshop/scripts
 
 #activate sicb2023 environment
 source $HOME/miniconda3/bin/activate sicb2023
