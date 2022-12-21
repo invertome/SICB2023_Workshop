ed6ed83463acc7d0a86f9c918e1d7f373b49d594
InverTome <104219631+invertome@users.noreply.github.com>
2022-12-21 10:10:10 -0700

    Update install_linux.sh

--- installation/install_linux.sh
+++ installation/install_linux.sh
@@ -50,6 +50,8 @@ conda config --set channel_priority strict
 conda init bash
 conda activate sicb2023
 
+conda install -c conda-forge coreutils
+
 # install programs
 programs=("biopython" "hyphy" "orthofinder" "transdecoder" "iqtree" "mafft" "clipkit" "hmmer" "paml" "treeshrink" "seqtk")
 for program in "${programs[@]}"
@@ -64,6 +66,27 @@ done
 # clone TIAMMAt
 cd $folder
 git clone https://github.com/mtassia/TIAMMAt.git
+
+mkdir Proteomes 
+mkdir PfamModels 
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+gzip -d *.gz #Decompress pfam database
+
+#Set-up Proteomes directory
+cd Proteomes/
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/605/GCF_000003605.2_Skow_1.1/GCF_000003605.2_Skow_1.1_protein.faa.gz #Download example proteome 1
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/224/145/GCF_000224145.3_KH/GCF_000224145.3_KH_protein.faa.gz #Download example proteome 2
+gzip -d * #Decompress downloaded proteoms
+
+#Set-up PfamModels directory
+cd ..
+cd PfamModels/
+
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz #Low-weight metadata used to get up-to-date accession for target domains
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.seed.gz #STOCKHOLM formatted sed alignments
+gzip -d *.gz #
+
+cd $folder
 
 # clone Workshop materials
 git clone https://github.com/invertome/SICB2023_Workshop.git
