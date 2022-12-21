4465c9ed852b5af63d895fbba3e00acbc4215026
InverTome <104219631+invertome@users.noreply.github.com>
2022-12-21 10:01:59 -0700

    Update install_macos.sh

--- installation/install_macos.sh
+++ installation/install_macos.sh
@@ -26,7 +26,9 @@ else
 
     #add python3 to path
     export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin
-
+    #add python3 to bashrc
+    echo "export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin" >> ~/.bashrc
+    
     #test if python3 was successfully installed
     if command -v python3 &> /dev/null; then
         echo "Python 3 has been successfully installed!"
@@ -55,6 +57,9 @@ else
 
     #add miniconda3 to path
     export PATH=$PATH:$HOME/miniconda3/bin
+    
+    #add miniconda3 to bashrc
+    echo "export PATH=$PATH:$HOME/miniconda3/bin" >> ~/.bashrc
 
     #test if miniconda3 was successfully installed
     if command -v conda &> /dev/null; then
@@ -77,6 +82,7 @@ conda config --set channel_priority strict
 #test if the following software is installed:
 #orthofinder, transdecoder, iqtree, mafft, clipkit, hmmer, hyphy, paml, perl, treeshrink, seqtk
 
+conda install -c conda-forge coreutils
 
 if command -v orthofinder &> /dev/null; then
     echo "Orthofinder is installed."
@@ -113,7 +119,7 @@ else
     echo "Clipkit installed."
 fi
 
-if command -v hmmer &> /dev/null; then
+if command -v hmmsearch &> /dev/null; then
     echo "Hmmer is installed."
 else
     conda install -n sicb2023 hmmer -y
@@ -127,7 +133,7 @@ else
     echo "Hyphy installed."
 fi
 
-if command -v paml &> /dev/null; then
+if command -v codeml &> /dev/null; then
     echo "Paml is installed."
 else
     conda install -n sicb2023 paml -y
@@ -150,11 +156,33 @@ fi
 
 conda install -n sicb2023 python=3.7 -y
 
+#activate sicb2023 environment
+conda init bash
+conda activate sicb2023
 
 cd $folder
 
 #clone the tiammat repository
 git clone https://github.com/mtassia/TIAMMAt.git
+
+mkdir Proteomes 
+mkdir PfamModels 
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+gzip -d *.gz #Decompress pfam database
+
+#Set-up Proteomes directory
+cd Proteomes/
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/605/GCF_000003605.2_Skow_1.1/GCF_000003605.2_Skow_1.1_protein.faa.gz #Download example proteome 1
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/224/145/GCF_000224145.3_KH/GCF_000224145.3_KH_protein.faa.gz #Download example proteome 2
+gzip -d * #Decompress downloaded proteoms
+
+#Set-up PfamModels directory
+cd ..
+cd PfamModels/
+
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz #Low-weight metadata used to get up-to-date accession for target domains
+wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.seed.gz #STOCKHOLM formatted sed alignments
+gzip -d *.gz #
 
 #clone the workshop materials
 git clone https://github.com/invertome/SICB2023_Workshop.git
@@ -162,9 +190,8 @@ git clone https://github.com/invertome/SICB2023_Workshop.git
 #add the location of python, miniconda3, and tiammat to path
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin:$HOME/miniconda3/bin:$folder/TIAMMAt:$folder/SICB2023_Workshop/scripts
 
-#activate sicb2023 environment
-conda init bash
-conda activate sicb2023
+#add miniconda3 to bashrc
+echo "export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin:$HOME/miniconda3/bin:$folder/TIAMMAt:$folder/SICB2023_Workshop/scripts" >> ~/.bashrc
 
 #test if the programs are now able to run
 
