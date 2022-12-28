1f02ed5dd41ba6352e1cb6599df25cf8cc2f9a4f
InverTome <104219631+invertome@users.noreply.github.com>
2022-12-28 10:40:37 -0700

    Update install_macosM1.sh

--- installation/install_macosM1.sh
+++ installation/install_macosM1.sh
@@ -164,6 +164,7 @@ conda activate sicb2023
 conda install -y -n sicb2023 -c bioconda perl-db-file
 conda install -y -n sicb2023 -c conda-forge r-ape
 conda install -y -n sicb2023 -c bioconda perl-uri
+conda install -y -n sicb2023 pal2nal
 cpan URI::Escape
 pip install --user phylopypruner
 
